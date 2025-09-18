daa4bad034354c1c6a78ea702967d21714c30c96
TOMAZ MACHADO DE AQUINO <128157969+Tomazoo1@users.noreply.github.com>
2025-09-18 12:21:14 -0300

    codigo java script

--- script.js
+++ script.js
@@ -11,4 +11,29 @@ function adicionarProduto(){
 
     produtos.push(produto);
 
+    mostrarProdutos();
+
+    document.getElementById('nome').value = "";
+    document.getElementById('marca').value = "";
+    document.getElementById('preco').value = "";
+    document.getElementById('foto').value = "";
+    document.getElementById('descricao').value = "";
+
+}
+
+function mostrarProdutos(){
+    const lista = document.getElementById('lista-produtos');
+    lista.innerHTML = "";
+
+    produtos.forEach((produto) => {
+       lista.innerHTML +=
+       <div class="produto">
+        <img src= "${produto.foto}" alt = "Imagem do produto"></img>
+        <div class="produto-info">
+            <h3> ${produto.nome} - ${produto.marca}</h3>
+            <p> <strong>Preço: </strong> R$ ${parseFloat(produto.preco).toFixed(2)} </p>
+            <p> ${produto.descricao}</p>
+        </div>
+       </div>
+    });
 }
